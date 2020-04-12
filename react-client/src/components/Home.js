e3fe3f4427e981371caa8bbd12d4c649d70f4ff0
Sadia Rashid <sadia.rashid1801@gmail.com>
2020-04-12 13:04:39 -0400

    commit

--- react-client/src/components/Home.js
+++ react-client/src/components/Home.js
@@ -6,10 +6,9 @@ function Home(props) {
     return (
         <div className="container">
             <div className="span12 div-style">
-                <h2 className="h2-style"> Centennial College Student Course App</h2>
+                <h2 className="h2-style"> Centennial College Nurse Patient App</h2>
                 <p className="p-style">
-                    React front-end calls Express REST API to add, list, update, or delete
-                    a user, create a course etc.
+                    React front-end calls Express REST API.
         </p>
             </div>
         </div>
